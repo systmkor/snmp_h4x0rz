886726d6c12dd795e25ff9b8a25b1c2c1cb7625b
systmkor <systmkor@gmail.com>
2012-06-05 02:09:30 -0700

    Kraking works in a basic form given the snmp data file and the dicitonary file

--- tools/hmac-crack/krak.py
+++ tools/hmac-crack/krak.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 ####
 #
 # KRAK
@@ -8,21 +10,55 @@
 # Author: Orion Miller
 #
 
-#!/usr/bin/pythin
+
+class cli_options:
+  passwd_out_path=''
+  stdout = True
+  pass_dict_path = ''
+
+class snmp_info:
+  snmp_filepath =''
+  snmp_file = None
+  msg = ''
+  digest = ''
+
+  def get_info(self):
+    self.snmp_file = open(self.snmp_filepath, 'rb')
+    self.digest = self.snmp_file.read(24)
+    self.snmp_file.read(1) #passed the delim
+    self.msg = self.snmp_file.read(1024) #crappy way of reading msg
 
 import hmac
-pass_dict = ''
-msg = ''
-digest = ''
-curr_digest = ''
+
+def main():
+   cli_opt = cli_options()
+   cli_opt.pass_dict_path = raw_input("Password Dictionary File Path: ") 
+   info = snmp_info()
+   info.snmp_filepath = raw_input("SNMP information file path: ")
+   info.get_info()
+   print(info.digest)
+   print(info.msg)
+   dict_krak(info.msg, info.digest, cli_opt.pass_dict_path)
+
 
 def dict_krak(msg='', digest='', pass_dict=''):
-  for line in open(pass_dict, 'r')
-    if hmac.new(key=line, msg).hexdigest() == digest:
+  for line in open(pass_dict, 'r'):
+    #print line,
+    #print hmac.new(line[:-1], msg).hexdigest()[0:24]
+    #print digest
+    if hmac.new(line[:-1], msg).hexdigest()[0:24] == digest:
       print line
 
 def print_password(passwd='', pathname='', stdout=True):
-  
+  if pathname is not '':
+    f = open(pathname, 'w')
+    f.write(passwd)
+    f.close()
+
   if stdout:
     print(passwd)
+
+
+if __name__ == "__main__":
+  main()
 
